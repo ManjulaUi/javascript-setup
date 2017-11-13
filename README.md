283f2cb1e7b93ecab7693a396a34afbb2a93c727
srinivas kasiriveni <kasiriveni@gmail.com>
2017-11-13 10:21:50 +0530

    adding the testcase setup

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # javascript-setup
 sample demo app
+
+```
+npm install
+npm start
+npm test
+```
---
+++ spec/js/app.spec.js
@@ -0,0 +1,5 @@
+describe('A sute for app',function(){
+    it('testing the student function',function(){
+        expect(true).toBe(true)
+    })
+});
